ad0dcf48bc5ef50deb99ae5fed6245bf95f1c4c1
yhjeong-C <yhjeong@bimatrix.co.kr>
2017-06-22 13:33:27 +0900

    selection 추가중

--- webapp/js/core/appSelection.js
+++ webapp/js/core/appSelection.js
@@ -19,8 +19,54 @@
 })(typeof window !== 'undefined' ? window : this, function(publicObject, eventObject, mixins) {
 
     Vue.component('app-selection', {
+        props: ['item'],
         mixins: [mixins.getPanelMixin()],
-        template: '#app-selection-template'
+        data: function() {
+            return {
+                children: [
+                    {
+                        direction: 'ne',
+                        type: 'close'
+                    },
+                    {
+                        direction: 'e',
+                        type: 'arrow'
+                    },
+                    {
+                        direction: 'se',
+                        type: 'copy'
+                    },
+                    {
+                        direction: 'sw',
+                        type: 'rotation'
+                    },
+                    {
+                        direction: 'w',
+                        type: 'cut'
+                    }
+                ]
+            };
+        },
+        template: '#app-selection-template',
+        computed: {
+
+        },
+        methods: {
+            itemClassObject: function(item) {
+                return {
+                    'app-icon-selection-close': item.type == 'close',
+                    'app-icon-selection-arrow': item.type == 'arrow',
+                    'app-icon-selection-copy': item.type == 'copy',
+                    'app-icon-selection-rotation': item.type == 'rotation',
+                    'app-icon-selection-cut': item.type == 'cut',
+
+
+                };
+            },
+            onClickLeft: function(e, item) {
+                console.log('app-selection', 'onClickLeft', item.type, eventObject.ActiveObject.item.id);
+            }
+        }
     });
 
     return {};
