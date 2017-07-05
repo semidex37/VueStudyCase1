22367ed02facdada31b5c06497151bc1b81a6b39
yhjeong-C <yhjeong@bimatrix.co.kr>
2017-07-05 17:25:23 +0900

    모듈 팝업의 템플릿 작업

--- webapp/js/module/popup/popup.js
+++ webapp/js/module/popup/popup.js
@@ -15,26 +15,26 @@
 })(typeof window !== 'undefined' ? window : this, function(templateObject) {
 
     Vue.component('app-popup-container-template', {
-        template: '#app-popup-container-template'
+        template: templateObject['app-popup-container-template']
     });
 
 
     Vue.component('app-popup-header-template', {
-        template: '#app-popup-header-template'
+        template: templateObject['app-popup-header-template']
     });
 
     Vue.component('app-popup-body-template', {
-        template: '#app-popup-body-template'
+        template: templateObject['app-popup-body-template']
 
     });
 
     Vue.component('app-popup-footer-template', {
-        template: '#app-popup-footer-template'
+        template: templateObject['app-popup-footer-template']
 
     });
 
     Vue.component('app-popup-buttons-template', {
-        template: '#app-popup-buttons-template',
+        template: templateObject['app-popup-buttons-template'],
         data: function() {
             return {
                 button: {
@@ -47,17 +47,17 @@
 
     Vue.component('app-popup-button-template', {
         props: ['type'],
-        template: '#app-popup-button-template'
+        template: templateObject['app-popup-button-template']
 
     });
 
     Vue.component('app-popup-search-template', {
-        template: '#app-popup-search-template'
+        template: templateObject['app-popup-search-template']
 
     });
 
     Vue.component('app-popup-tree-template', {
-        template: '#app-popup-tree-template'
+        template: templateObject['app-popup-tree-template']
     });
 
     new Vue({
--- webapp/js/template/module/templateObject.js
+++ webapp/js/template/module/templateObject.js
@@ -6,20 +6,50 @@
     // export as AMD...
     if(typeof define !== 'undefined' && define.amd) {
         define([
-            loadText+'popup/app-popup-template.html'
+            loadText+'module/popup/app-popup-body-template.html',
+            loadText+'module/popup/app-popup-button-template.html',
+            loadText+'module/popup/app-popup-buttons-template.html',
+            loadText+'module/popup/app-popup-container-template.html',
+            loadText+'module/popup/app-popup-footer-template.html',
+            loadText+'module/popup/app-popup-header-template.html',
+            loadText+'module/popup/app-popup-search-template.html',
+            loadText+'module/popup/app-popup-tree-template.html'
             ], factory);
     }else {
         global.templateObject = factory([
-            global['app-popup-template']
+            global['app-popup-template'],
+            global['app-popup-body-template'],
+            global['app-popup-button-template'],
+            global['app-popup-buttons-template'],
+            global['app-popup-container-template'],
+            global['app-popup-footer-template'],
+            global['app-popup-header-template'],
+            global['app-popup-search-template'],
+            global['app-popup-tree-template']
+            
         ]);
     }
 
 })(typeof window !== 'undefined' ? window : this, function(
-        appPopupTemplate
+    appPopupBodyTemplate,
+    appPopupButtonTemplate,
+    appPopupButtonsTemplate,
+    appPopupContainerTemplate,
+    appPopupFooterTemplate,
+    appPopupHeaderTemplate,
+    appPopupSearchTemplate,
+    appPopupTreeTemplate
 ) {
 
     var templateObject = {
-        'app-popup-template': appPopupTemplate
+        'app-popup-body-template': appPopupBodyTemplate,
+        'app-popup-button-template': appPopupButtonTemplate,
+        'app-popup-buttons-template':   appPopupButtonsTemplate,
+        'app-popup-container-template': appPopupContainerTemplate,
+        'app-popup-footer-template': appPopupFooterTemplate,
+        'app-popup-header-template': appPopupHeaderTemplate,
+        'app-popup-search-template': appPopupSearchTemplate,
+        'app-popup-tree-template': appPopupTreeTemplate
     };
 
     return templateObject;
